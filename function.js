460aac06aee7e70c2a740d58dd5ef4ceb855bb57
Uhmar Vince Aceron <aceronuhmarvince@gmail.com>
2025-01-13 22:39:56 +0800

    a

---
+++ function.js
@@ -0,0 +1,31 @@
+let rollDown = document.getElementById('head2');
+
+rollDown.addEventListener('click', rollingThunder);
+
+function rollingThunder() {
+  document.getElementById("secondPart").scrollIntoView({ behavior: "smooth" });
+}
+
+let rollDown1 = document.getElementById('head3');
+
+rollDown1.addEventListener('click', rollingThunder1);
+
+function rollingThunder1() {
+  document.getElementById("fourthPart").scrollIntoView({ behavior: "smooth" });
+}
+
+let rollDown2 = document.getElementById('head4');
+
+rollDown2.addEventListener('click', rollingThunder2);
+
+function rollingThunder2() {
+  document.getElementById("fifthPart").scrollIntoView({ behavior: "smooth" });
+}
+
+let rollDown3 = document.getElementById('head5');
+
+rollDown3.addEventListener('click', rollingThunder3);
+
+function rollingThunder3() {
+  document.getElementById("sixthPart").scrollIntoView({ behavior: "smooth" });
+}
