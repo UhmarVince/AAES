21df6ec8ed76fc5fd5a3fbdd8a7696accfe844ae
Uhmar Vince Aceron <aceronuhmarvince@gmail.com>
2025-01-13 10:47:16 +0800

    a

--- animation.js
+++ animation.js
@@ -52,3 +52,56 @@ function shuffleText5(element, text, duration = 2000) {
 
 shuffleText(textElement5, originalText5);
 
+const textElement6 = document.getElementById("head6");
+const originalText6 = textElement6.textContent;
+
+function shuffleText6(element, text, duration = 2000) {
+  const characters = '!@#$%^&*()';
+  const textArray = text.split("");
+  const iterations = text.length * 3; // How many shuffles per character
+  let counter = 0;
+
+  const interval = setInterval(() => {
+    const newText = textArray.map((char, i) => {
+      if (counter > i * iterations / textArray.length) return char;
+      return characters.charAt(Math.floor(Math.random() * characters.length));
+    }).join("");
+
+    element.textContent = newText;
+
+    counter += textArray.length;
+    if (counter > iterations) {
+      clearInterval(interval);
+      element.textContent = text; // Reset to the original text
+    }
+  }, 50);
+}
+
+shuffleText(textElement6, originalText6);
+
+const textElement7 = document.getElementById("head7");
+const originalText7 = textElement7.textContent;
+
+function shuffleText7(element7, text, duration = 2000) {
+  const characters7 = '!@#$%';
+  const textArray7 = text7.split("");
+  const iterations7 = text7.length * 1; // How many shuffles per character
+  let counter = 0;
+
+  const interval7 = setInterval(() => {
+    const newText7 = textArray7.map((char, i) => {
+      if (counter > i * iterations7 / textArray7.length) return char;
+      return characters7.charAt(Math.floor(Math.random() * characters7.length));
+    }).join("");
+
+    element7.textContent = newText7;
+
+    counter += textArray7.length;
+    if (counter > iterations7) {
+      clearInterval(interval7);
+      element7.textContent = text; // Reset to the original text
+    }
+  }, 50);
+}
+
+shuffleText(textElement7, originalText7);
